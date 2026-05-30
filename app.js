db31b88b076caa44bcafb053d19f1960ca3e151e
Harekrushna Pradhan <hkrushnapradhan@gmail.com>
2026-05-30 15:33:22 +0530

    Ready to deploy

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 const mysql = require("mysql2");
 
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  passwordExists: !!process.env.DB_PASSWORD
+});
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
