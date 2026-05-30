7e01a4dc87b2d708aa3cddf778d1ba9eafaab83a
Harekrushna Pradhan <hkrushnapradhan@gmail.com>
2026-05-30 15:37:11 +0530

    Resolve merge conflicts in env and db config

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql2");
 
-<<<<<<< HEAD
 console.log({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -8,13 +7,6 @@ console.log({
   database: process.env.DB_NAME,
   passwordExists: !!process.env.DB_PASSWORD
 });
-=======
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
->>>>>>> 3ee3ace8ef000b6829420567c7012992134f1515
-
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
